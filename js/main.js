b58f1210a553eb444abe80e1b1564fd7c7d23f06
Lluís <lluiscab@gmail.com>
2019-11-27 17:38:52 +0100

    Optimized canvas drawing

--- js/main.js
+++ js/main.js
@@ -1,21 +1,44 @@
 const pad = (n, width, z) => {
-	z = z || "0";
-	n = n + "";
-	return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+  z = z || "0";
+  n = n + "";
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 };
 
 // Marc's magic ^o^
 let scene = new Scene();
 
 let cam = new Camera(new CamSettings(2, 1, 600, 300));
+let triangle1 = new Triangle([
+  new Vector([2, 0, 0]),
+  new Vector([0, 2, 0]),
+  new Vector([0, 0, 2])
+]);
+let triangle2 = triangle1.clone();
+triangle2.tr.translate(new Vector([0, 5, 0]));
+let nul = new Null();
+nul.tr.translate(new Vector([1, 0, 0.5]));
+nul.add([triangle2]);
 
-scene.add([cam]);
+triangle1.tr.translate(new Vector([6, -0.5, 1]));
+triangle1.tr.rotateZ(Math.PI / 2);
+
+let mesh = new Mesh();
+mesh.add([triangle1]);
+mesh.tr.translate(new Vector([-2, 3, 0]));
+mesh.tr.rotateZ(Math.PI / 2);
+
+scene.add([cam, mesh, nul]);
 
 let rendering = false;
 const renderFrame = () => {
-	// Prevent new renders if already rendering
-	if (rendering) throw new Error("Already rendering");
-	rendering = true;
+  const drawPixel = (canvas, pixel, x, y, w, h) => {
+    canvas.fillStyle = `#${pad(new Number(pixel).toString(16), 6)}`;
+    canvas.fillRect(x, y, w, h);
+  };
+
+  // Prevent new renders if already rendering
+  if (rendering) throw new Error("Already rendering");
+  rendering = true;
 
   // Get frame
   let getFrameT1 = performance.now();
@@ -23,36 +46,49 @@ const renderFrame = () => {
   let getFrameT2 = performance.now();
   console.log("Render took " + (getFrameT2 - getFrameT1) + " milliseconds.");
 
-	// Get frame
-	let getFrameT1 = performance.now();
-	let frame = cam.getFrame();
-	let getFrameT2 = performance.now();
-	console.log("Render took " + (getFrameT2 - getFrameT1) + " milliseconds.");
+  // Get canvas
+  const canvasElement = document.getElementById("canvas");
+  const canvas = canvasElement.getContext("2d");
+  canvasElement.width = 600;
+  canvasElement.height = 300;
 
-	// Get canvas
-	const canvasElement = document.getElementById("canvas");
-	const canvas = canvasElement.getContext("2d");
-	canvasElement.width = 600;
-	canvasElement.height = 300;
+  // Render on canvas
+  let canvasDrawT1 = performance.now();
+  for (let j = 0; j < frame.length; j++) {
+    // If all the same, draw single line
+    if (frame[j].every(pixel => pixel.me === frame[j][0].me)) {
+      drawPixel(canvas, frame[j][0].me, 0, j, frame[j].length, 1);
+      continue;
+    }
+    // If not all the same, draw in batch
+    let prevPixelPos = 0;
+    let prevPixel = frame[j][0].me;
 
-	// Render on canvas
-	let canvasDrawT1 = performance.now();
-	for (let j = 0; j < frame.length; j++)
-		for (let i = 0; i < frame[0].length; i++) {
-			const pixel = frame[j][i].me;
-			canvas.fillStyle = `#${pad(new Number(pixel).toString(16), 6)}`;
-			canvas.fillRect(i, j, 1, 1);
-		}
-	let canvasDrawT2 = performance.now();
+    for (let i = 0; i < frame[0].length; i++) {
+      const pixel = frame[j][i].me;
 
-	console.log(
-		"Draw on canvas took " + (canvasDrawT2 - canvasDrawT1) + " milliseconds."
-	);
+      // If pixel changed mid way throught render
+      if (pixel != prevPixel) {
+        drawPixel(canvas, prevPixel, prevPixelPos, j, i, 1);
+        prevPixel = pixel;
+        prevPixelPos = i;
+      }
+      // If reached the end
+      else if (i === frame[0].length - 1)
+        drawPixel(canvas, prevPixel, prevPixelPos, j, i, 1);
+    }
+  }
 
-	console.log(cam);
+  let canvasDrawT2 = performance.now();
 
-	// Allow new renders
-	rendering = false;
+  console.log(
+    "Draw on canvas took " + (canvasDrawT2 - canvasDrawT1) + " milliseconds."
+  );
+
+  console.log(cam);
+
+  // Allow new renders
+  rendering = false;
 };
 
 const readUploadedFileAsText = inputFile => {
@@ -82,11 +118,11 @@ const loadObjFile = async event => {
   const lines = text.split(/[\r\n]+/g);
 
   // Create mesh
-	let mesh = new Mesh();
-	
-	// Create data storage
-	const vectors = [];
-	const triangles = [];
+  let mesh = new Mesh();
+
+  // Create data storage
+  const vectors = [];
+  const triangles = [];
 
   // Process file
   lines.forEach(line => {
@@ -97,25 +133,24 @@ const loadObjFile = async event => {
     const type = data[0];
     const values = data.slice(1).map(i => parseFloat(i));
 
-		if(type === 'v') {
-			vectors.push(new Vector(values))
-			return;
-		}
+    if (type === "v") {
+      vectors.push(new Vector(values));
+      return;
+    }
 
-		if (type === "f") {
-			triangles.push(
+    if (type === "f") {
+      triangles.push(
         new Triangle([
           vectors[values[0] - 1].clone(),
           vectors[values[1] - 1].clone(),
           vectors[values[2] - 1].clone()
         ])
-			);
+      );
       return;
     }
+  });
 
-	});
-	
-	mesh.add(triangles);
+  mesh.add(triangles);
 
   // Add mest to scene and render
   scene.add([mesh]);
